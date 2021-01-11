acc0ec6ae112cd87498eff992f8250b3b96b38b4
Carlos Dagorret <dagorret@fce.unrc.edu.ar>
2021-01-11 15:41:55 -0300

    Crear lista, acceder elementos y porciones de listas

---
+++ practica_listas.py
@@ -0,0 +1,14 @@
+miLista = ["María", "Pepe", "Marta", "Antonio"]
+
+# Imprime toda la lista
+print(miLista)
+
+#imprime el 3er elemento
+print(miLista[2])
+
+#imprime el último elemento por ser negativo. Empieza a contar desde atrás
+print(miLista[-1])
+
+#Imprimir a porciiones de la lista
+#El primer elemento es inclusivo y el segundo elemento es exclusivo
+print(miLista[0:3])
