291a2c2936c12ef23f7829a071b5e3fff96aefd0
Carlos Dagorret <dagorret@fce.unrc.edu.ar>
2021-01-13 23:20:50 -0300

    Crear una clase

---
+++ ppo_1.py
@@ -0,0 +1,21 @@
+class Coche():
+    largo_chasis = 250
+    ancho_chasis = 120
+    ruedas = 4
+    en_marcha = False
+
+    def arrancar(self):
+        self.en_marcha = True
+
+    def estado(self):
+        if self.en_marcha:
+            return "El coche está en marcha"
+        else:
+            return "El coche está parado"
+
+mi_coche = Coche()
+print("El largo del coche es: ", mi_coche.largo_chasis)
+print("El coche tiene:", mi_coche.ruedas," ruedas")
+
+mi_coche.arrancar()
+print(mi_coche.estado())
