bab176524b50065db68ad66228d399d6b70474f6
Carlos Dagorret <dagorret@fce.unrc.edu.ar>
2021-01-15 15:03:40 -0300

    Create chequeo interno not encasulapte function

--- ppo_3.py
+++ ppo_3.py
@@ -6,10 +6,16 @@ class Coche():
         self.__ruedas = 4 ##protección de la propiedad
         self.__en_marcha = False
 
-    def arrancar(self, arracamos):
-        self.__en_marcha = arracamos
+    def arrancar(self, arrancamos):
+        self.__en_marcha = arrancamos
+
         if self.__en_marcha:
+            chequeo = self.chequeo_interno()
+
+        if self.__en_marcha and chequeo:
             return "El coche está en marcha"
+        elif self.__en_marcha and chequeo == False:
+            return "Algo ha ido mal en el chequeo. No podemos arrancar"
         else:
             return "El coche está parado"
 
@@ -17,6 +23,20 @@ class Coche():
         print("El cocche tiene ", self.__ruedas, " ruedas.",
                 "Un ancho de ", self.__ancho_chasis,
                 " y un largo de ", self.__largo_chasis)
+    
+    def chequeo_interno(self):
+        print("Realizando Chequeo Interno")
+
+        self.gasolina = "ok"
+        self.aceite = "ok"
+        self.puertas = "cerradas"
+
+        if (self.gasolina == "ok" and self.aceite == "ok" and 
+            self.puertas == "cerradas"):
+            return True
+        else:
+            return False
+        
 
 mi_coche = Coche()
 
@@ -27,5 +47,4 @@ print("-----------Segundo objecto--------------")
 
 mi_coche_2 = Coche()
 print(mi_coche_2.arrancar(False))
-mi_coche_2.__ruedas = 5 ## Esta oculto la propiedad, pero es accesible mediante dir()
 mi_coche_2.estado()
