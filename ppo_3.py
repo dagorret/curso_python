40f56bdc29682f65ef4cf8b27d17044d233f1dce
Carlos Dagorret <dagorret@fce.unrc.edu.ar>
2021-01-14 21:39:21 -0300

    add vs code support

---
+++ ppo_3.py
@@ -0,0 +1,31 @@
+class Coche():
+
+    def __init__(self):
+        self.__largo_chasis = 250
+        self.__ancho_chasis = 120
+        self.__ruedas = 4 ##protección de la propiedad
+        self.__en_marcha = False
+
+    def arrancar(self, arracamos):
+        self.__en_marcha = arracamos
+        if self.__en_marcha:
+            return "El coche está en marcha"
+        else:
+            return "El coche está parado"
+
+    def estado(self):
+        print("El cocche tiene ", self.__ruedas, " ruedas.",
+                "Un ancho de ", self.__ancho_chasis,
+                " y un largo de ", self.__largo_chasis)
+
+mi_coche = Coche()
+
+print(mi_coche.arrancar(True))
+mi_coche.estado()
+
+print("-----------Segundo objecto--------------")
+
+mi_coche_2 = Coche()
+print(mi_coche_2.arrancar(False))
+mi_coche_2.__ruedas = 5 ## Esta oculto la propiedad, pero es accesible mediante dir()
+mi_coche_2.estado()
