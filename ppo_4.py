69f9c7ab3ed13087da7f61699cb9436d616e19d0
Carlos Dagorret <dagorret@fce.unrc.edu.ar>
2021-01-15 15:30:06 -0300

    Create Vehiculo Class

---
+++ ppo_4.py
@@ -0,0 +1,24 @@
+class Vehiculos():
+
+    def __init__(self, marca, modelo):
+        self.marca = marca 
+        self.modelo = modelo 
+        self.en_marcha = False
+        self.acelerar = False
+        self.frenar = False
+
+    def arrancar(self):
+        self.en_marcha = True
+
+    def acelerar(self):
+        self.acelerar = True
+
+    def frenar(self):
+        self.frenar = True
+
+    def estado(self):
+        print("Marca:", self.marca, "\n",
+            "Modelo: ", self.modelo, "\n",
+            "En Marcha: ", self.en_marcha, "\n",
+            "Acelerando: ", self.acelerar, "\n",
+            "Frenando: ", self.frenar, "\n")
