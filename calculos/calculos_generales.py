d121a432f43857078dd1c6b19dfba124d18723a6
Carlos Dagorret <dagorret@fce.unrc.edu.ar>
2021-01-16 18:59:48 -0300

    Paquetes

---
+++ calculos/calculos_generales.py
@@ -0,0 +1,17 @@
+def sumar(a,b):
+    print("La suma es: ", a + b)
+
+def restar(a,b):
+    print("La resta es: ", a - b)
+
+def multiplicar(a,b):
+    print("La multiplicacion es: ", a * b)
+
+def dividir(a,b):
+    print("La division es: ", a / b)
+
+def potencia(a,b):
+    print("La potencia es: ", a ** b)
+
+def redondear(a):
+    print("El número redondeado es: ", round(a))
---
+++ uso_modulos.py
@@ -0,0 +1,4 @@
+from calculos.calculos_generales import dividir, potencia
+
+dividir(8,2)
+potencia(4,8)
