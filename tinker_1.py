75721a63f18c7bb09aa891618562be0c6dd1b47a
Carlos Dagorret <dagorret@fce.unrc.edu.ar>
2021-01-16 23:38:32 -0300

    GUI Parte 2

--- tinker_1.py
+++ tinker_1.py
@@ -5,11 +5,19 @@ import sys
 raiz = Tk()
 
 raiz.title("Ventana de Pruebas")
-raiz.resizable(True, False)
+raiz.resizable(True, True)
 p1 = PhotoImage(file = 'icono.png')
 raiz.iconphoto(True, p1)
 
-raiz.geometry("650x350")
+#raiz.geometry("650x350")
 
 raiz.config(bg="blue")
+
+frame = Frame()
+frame.pack(fill="both", expand=True)
+frame.config(bg="red")
+frame.config(width="650", height="350")
+frame.config(bd=34)
+frame.config(relief="groove")
+frame.config(cursor="pirate")
 raiz.mainloop()
