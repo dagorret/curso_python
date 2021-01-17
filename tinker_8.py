be4fcc424c72230e4f0c31799735fe7393ddf971
Carlos Dagorret <dagorret@fce.unrc.edu.ar>
2021-01-17 19:07:17 -0300

    Ventanas Emergentes

--- tinker_8.py
+++ tinker_8.py
@@ -1,6 +1,24 @@
 from tkinter import *
+from tkinter import messagebox
 
 root = Tk()
+
+def InfoAdicional():
+    messagebox.showinfo("Procesador", "Procesador versión 1.0")
+
+def InfoLicencia():
+    messagebox.showwarning("Licencia", "Producto bajo licencia GNU")
+
+def Salir():
+    #valor = messagebox.askquestion("Salir", "¿Desea salir de la apliacación?")
+    valor = messagebox.askokcancel("Salir", "¿Desea salir de la apliacación?")
+    if valor:
+        root.destroy()
+
+def Cerrar():
+    valor = messagebox.askretrycancel("Reintentar", "No es posible cerrar")
+    if not valor:
+        root.destroy()
 
 barramenu = Menu(root)
 root.config(menu=barramenu, width = 300, height=300)
@@ -10,8 +28,8 @@ archivo_menu.add_command(label = "Nuevo")
 archivo_menu.add_command(label = "Guardar")
 archivo_menu.add_command(label = "Guardar como")
 archivo_menu.add_separator()
-archivo_menu.add_command(label = "Cerrar")
-archivo_menu.add_command(label = "Salir")
+archivo_menu.add_command(label = "Cerrar", command=Cerrar)
+archivo_menu.add_command(label = "Salir", command=lambda:Salir())
 
 archivo_edicion_menu = Menu(barramenu, tearoff = 0)
 archivo_edicion_menu.add_command(label = "Copiar")
@@ -19,8 +37,8 @@ archivo_edicion_menu.add_command(label = "Cortar")
 archivo_edicion_menu.add_command(label = "Pegar")
 archivo_herramientas_menu = Menu(barramenu)
 archivo_ayuda = Menu(barramenu, tearoff = 0)
-archivo_ayuda.add_command(label = "Licencia")
-archivo_ayuda.add_command(label = "Acerca de ..")
+archivo_ayuda.add_command(label = "Licencia", command=InfoLicencia)
+archivo_ayuda.add_command(label = "Acerca de ..", command=InfoAdicional)
 
 barramenu.add_cascade(label = "Archivo", menu = archivo_menu)
 barramenu.add_cascade(label = "Edición", menu = archivo_edicion_menu)
