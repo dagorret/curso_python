54020c83185d4970e019305bc20ea3f5ad3a4716
Carlos Dagorret <dagorret@fce.unrc.edu.ar>
2021-01-16 21:27:59 -0300

    Crear objeeto y mostrarlo

--- info_permanente.py
+++ info_permanente.py
@@ -12,4 +12,23 @@ class Persona:
     def __str__(self):
         return "{} {} {}".format(self.nombre, self.genero, self.edad)
 
-p = Persona("Carlos", "Femenino", 29)
+
+class ListaPersonas():
+    personas = []
+
+    def agregar(self, p):
+        self.personas.append(p)
+
+    def mostrar(self):
+        for p in self.personas:
+            print(p)
+
+miLista = ListaPersonas()
+p = Persona("Sandra", "Femenino", 29)
+miLista.agregar(p)
+p = Persona("Antonio", "Masculini", 33)
+miLista.agregar(p)
+p = Persona("Ana", "Femenino", 19)
+miLista.agregar(p)
+
+miLista.mostrar()
