cb553c2d541dfea289c3dc566d913e45fe955244
Carlos Dagorret <dagorret@fce.unrc.edu.ar>
2021-01-17 23:16:00 -0300

    Cuadros de Textos

--- app.py
+++ app.py
@@ -33,6 +33,31 @@ barramenu.add_cascade(label="CRUD", menu=crud_menu)
 barramenu.add_cascade(label="Ayuda", menu=ayuda_menu)
 
 # Campos
+mi_frame = Frame(root)
+mi_frame.pack()
+
+cuadro_id = Entry(mi_frame)
+cuadro_id.grid(row=0, column=1, padx=6, pady=6)
+
+cuadro_nombre = Entry(mi_frame)
+cuadro_nombre.grid(row=1, column=1, padx=6, pady=6)
+cuadro_nombre.config(fg="red", justify="right")
+
+cuadro_pass = Entry(mi_frame)
+cuadro_pass.grid(row=2, column=1, padx=6, pady=6)
+cuadro_pass.config(show="*")
+
+cuadro_apellido = Entry(mi_frame)
+cuadro_apellido.grid(row=3, column=1, padx=6, pady=6)
+
+cuadro_direccion = Entry(mi_frame)
+cuadro_direccion.grid(row=4, column=1, padx=6, pady=6)
+
+cuadro_texto = Text(mi_frame, width=16, height=5)
+cuadro_texto.grid(row=5, column=1, padx=6, pady=6)
+scroll_ver = Scrollbar(mi_frame, command=cuadro_texto.yview)
+scroll_ver.grid(row=5, column=2, sticky="nsew")
+cuadro_texto.config(yscrollcommand = scroll_ver.set)
 
 
 root.mainloop()
